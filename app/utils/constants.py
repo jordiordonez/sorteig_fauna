5555ea9ee19b5b3f427729dddf51aadbc6574e5f
jordiordonez <jordiordonez@hotmail.com>
2025-07-18 16:26:13 +0200

    Refactor app into package and optimize loading

---
+++ app/utils/constants.py
@@ -0,0 +1,26 @@
+ESPECIE_SORTEIGS = {
+    "Isard": ["IS TCC", "IS VCRS", "IS VCX", "IS VCE"],
+    "Cabirol": ["CAB"],
+    "Mufló": ["MUF UGEO", "MUF UGC", "MUF VTE-E", "MUF VCE", "MUF R"],
+}
+
+VEDAT_PARRÒQUIES = {
+    "IS VCE": {
+        "La Massana": 0.234,
+        "Sant Julià de Lòria": 0.241,
+        "Andorra la Vella": 0.522,
+        "Escaldes-Engordany": 0.003,
+    },
+    "IS VCRS": {"Canillo": 0.5, "Ordino": 0.5},
+    "IS VCX": {"La Massana": 1.0},
+}
+
+TIPUS_OPTIONS = [
+    "Femella",
+    "Mascle",
+    "Adult",
+    "Juvenil",
+    "Trofeu",
+    "Selectiu",
+    "Indeterminat",
+]
